cc5f6229a665df8bf3e25a53576200e28997b0f9
Noizzzz <Noizzzz@yandex.ru>
2023-08-27 23:51:37 +0700

    added task_1 for Homework_1

---
+++ HW_1/task_1/Answer.java
@@ -0,0 +1,26 @@
+package HW_1.task_1;
+
+class Answer {
+    public  void arrayOutOfBoundsException() {
+        // Напишите свое решение ниже
+        int[] arr = {1, 2, 3, 4};
+        System.out.println(arr[5]);
+
+    }
+
+    public  void divisionByZero() {
+        // Напишите свое решение ниже
+        int[] arr = {1, 2, 3, 4};
+        System.out.println(arr[0] / 0);
+
+    }
+
+    public  void numberFormatException() {
+        // Напишите свое решение ниже
+        String str = "Hello";
+        int num = Integer.parseInt(str);
+        System.out.println(num);
+
+
+    }
+}
