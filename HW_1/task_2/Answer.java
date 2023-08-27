49292698a51732d003b955b42b5c39f886b0691e
Noizzzz <Noizzzz@yandex.ru>
2023-08-28 00:18:03 +0700

    added task_2 for Homework_1

---
+++ HW_1/task_2/Answer.java
@@ -0,0 +1,20 @@
+package HW_1.task_2;
+
+class Answer {
+    public int[] subArrays(int[] a, int[] b) {
+        // Введите свое решение ниже
+        if (!compareLength(a, b)) {
+            return new int[1];
+        }else {
+            int[] result = new int[a.length];
+            for (int i = 0; i < a.length; i++) {
+                result[i] = a[i] - b[i];
+            }
+            return result;
+        }
+    }
+
+    public boolean compareLength(int[] a, int[] b) {
+        return a.length == b.length;
+    }
+}
